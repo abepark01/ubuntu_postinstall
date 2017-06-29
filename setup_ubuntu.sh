697e197e915726eabb1a4061d2c8e3d0b12f09d5
Abe <abepark01@users.noreply.github.com>
2017-06-29 08:12:32 -0700

    Update setup_ubuntu.sh
    add comments

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
+
+# setup global nodejs
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+
+# update apt packages
 sudo apt-get update
 sudo apt-get upgrade -y
+
+# install ruby pre-requisites
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# update global npm and install typescript and tern
+sudo npm install -g npm typescript tern
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+. ~/.bashrc
+nvm install 6.11.0
+nvm alias default 6.11.0
+
+# install rbenv
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 . ~/.bashrc
 
+# install ruby-build for rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
 
+# install ruby 2.4.1
 rbenv install 2.4.1
 rbenv global 2.4.1
 ruby -v
+
+# update gems
+gem update --system
+gem update
+gem install rails
+rbenv rehash
