7196e15bf57b16c6b1eca1c6eaef5846617448cb
Abe Park <abepark01@gmail.com>
2017-06-29 08:19:15 -0700

    add yarn and sublime text

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,12 +3,20 @@
 # setup global nodejs
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 
+# setup yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+# setup sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
 # update apt packages
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # install ruby pre-requisites
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake sublime-text yarn
 
 # update global npm and install typescript and tern
 sudo npm install -g npm typescript tern
@@ -41,3 +49,4 @@ gem update --system
 gem update
 gem install rails
 rbenv rehash
+
