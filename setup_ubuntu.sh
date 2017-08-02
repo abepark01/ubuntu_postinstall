f16dbbcd165106a848e6fbd16df94609e897c6be
Abe Park <abepark01@gmail.com>
2017-08-02 08:25:58 -0700

    python-pip

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -24,7 +24,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # install ruby pre-requisites
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake sublime-text yarn vim golang-go
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake sublime-text yarn vim golang-go python-pip
 
 # update global npm and install typescript and tern
 sudo npm install -g npm typescript tern
