5b74d90151dab1c19f14f9b3dac2923e8aa48154
Abe Park <abepark01@gmail.com>
2017-08-04 06:41:51 -0700

    postgres

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -13,6 +13,9 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
+# python 3.6
+sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+
 # vim
 sudo add-apt-repository ppa:pi-rho/dev -y
 
@@ -21,10 +24,11 @@ sudo add-apt-repository ppa:longsleep/golang-backports -y
 
 # update apt packages
 sudo apt-get update
+sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 
 # install ruby pre-requisites
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake sublime-text yarn vim golang-go python-pip
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake sublime-text yarn vim golang-go python-pip python3.6
 
 # update global npm and install typescript and tern
 sudo npm install -g npm typescript tern
@@ -32,3 +36,8 @@ cd $DIR
 
 ./setup_nvm.sh
 ./setup_ruby.sh
+./setup_postgresql.sh
+
+
+sudo apt-get autoclean
+sudo apt-get autoremove -y
