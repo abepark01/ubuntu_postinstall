6a6f1277aa7c45e9a6ad4f69316884f375de4e1c
Abe <abepark01@users.noreply.github.com>
2017-08-05 14:33:37 -0700

    Update setup_nvm.sh

--- setup_nvm.sh
+++ setup_nvm.sh
@@ -3,7 +3,7 @@ if [ ! -d ~/.nvm ];then
   echo "installing nvm..."
   cd $HOME
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-  . ~/.bashrc
+  source ~/.bashrc
   nvm install 8.2.1
   nvm alias default 8.2.1
 fi
