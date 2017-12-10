dbf6039d410a85ed15651f7e74420f069b1b022b
Abe <abepark01@users.noreply.github.com>
2017-12-10 01:26:35 -0800

    Update setup_nvm.sh

--- setup_nvm.sh
+++ setup_nvm.sh
@@ -1,13 +1,14 @@
 # install nvm
+NODE_VERSION=8.9.3
 echo "installing nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 8.2.1
-nvm alias default 8.2.1
+nvm install $NODE_VERSION
+nvm alias default $NODE_VERSION
 
-npm install -g npm tern typescript
+npm install -g npm tern typescript prettier gulp create-react-app
 source ~/.bashrc
