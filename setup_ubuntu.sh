a0312315afe8327a0633a317565a45f46afcd0e1
Abe <abepark01@users.noreply.github.com>
2017-12-12 22:05:26 -0800

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,16 +5,12 @@ sudo apt-get install software-properties-common -y
 # setup global nodejs
 DIR=$PWD
 cd $HOME
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 
 # setup yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-# setup sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # python 3.6
 sudo add-apt-repository ppa:jonathonf/python-3.6 -y
@@ -26,12 +22,12 @@ sudo add-apt-repository ppa:pi-rho/dev -y
 sudo add-apt-repository ppa:longsleep/golang-backports -y
 
 # update apt packages
-sudo apt-get update
+sudo apt-get update -qq
 sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 
 # install ruby pre-requisites
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake sublime-text yarn vim golang-go python-pip python3.6 ack-grep
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs python-dev cmake yarn vim python-pip python3.6 ack-grep
 
 # update global npm and install typescript and tern
 sudo npm install -g npm typescript tern
