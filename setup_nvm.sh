8c94ec11dd1e89a5481232c9764de4beff0e55e1
Abe Park <abepark01@gmail.com>
2017-12-13 12:59:29 -0800

    update nvm setup script

--- setup_nvm.sh
+++ setup_nvm.sh
@@ -1,7 +1,8 @@
 # install nvm
 NODE_VERSION=8.9.3
+NVM_VERSION=v0.33.8
 echo "installing nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
+curl -o- "https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh" | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
