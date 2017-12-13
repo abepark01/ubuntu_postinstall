beae45fcdcf754e7d1e2bd94fbada64037755022
Abe Park <abepark01@gmail.com>
2017-12-13 13:45:01 -0800

    fix scripts

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -2,12 +2,12 @@
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo -e '\nexport PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-. ~/.bashrc
 
 # install ruby-build for rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+
 rbenv install 2.4.2
 rbenv global 2.4.2
 ruby -v
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,8 +5,7 @@ apt-get install software-properties-common -y
 # setup global nodejs
 DIR=$PWD
 cd $HOME
-curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-. nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh | bash -
 
 # setup yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
