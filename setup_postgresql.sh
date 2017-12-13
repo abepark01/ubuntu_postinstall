dc50b9d3cc84008799f096d9624c386fb0ce5b3b
Abe Park <abepark01@gmail.com>
2017-12-13 13:07:17 -0800

    remove sudo

--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get install postgresql-9.6 libpq-dev -y
+add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update -qq
+apt-get install postgresql-9.6 libpq-dev -y
